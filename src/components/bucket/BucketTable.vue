95add422ac7fe4941f7529e8960b64639f190f1b
tscrond <tomasz.skrond@boar.network>
2025-05-07 01:08:28 +0200

    expandable drawer for personal files

--- src/components/bucket/BucketTable.vue
+++ src/components/bucket/BucketTable.vue
@@ -5,10 +5,10 @@
         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">File Name</th>
         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden md:table-cell">Content Type</th>
         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden md:table-cell">MD5 Checksum</th>
-        <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden md:table-cell">Size</th>
+        <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 md:table-cell">Size</th>
         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden md:table-cell">Date Created</th>
-        <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Download link</th>
-        <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Delete?</th>
+        <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Actions</th>
+        <!-- <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"></th> -->
     </tr>
     </thead>
     <tbody v-for="(object, i) in localObjectsList" :key="object.name" class="divide-y divide-neutral-700 overflow-scroll">
@@ -16,36 +16,11 @@
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm font-medium">{{ object.name }}</td>
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm hidden md:table-cell">{{ object.content_type }}</td>
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm hidden md:table-cell">{{ object.md5 }}</td>
-            <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm hidden md:table-cell">{{ formatSize(object.size) }}</td>
+            <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm md:table-cell">{{ formatSize(object.size) }}</td>
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm hidden md:table-cell">{{ formatDateToShortString(object.date_created) }}</td>
+            
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm">
-                <a type="button" 
-                v-if="fileLinks[object.name]"
-                :href="`${fileLinks[object.name]}`"
-                download
-                class="
-                break-all whitespace-normal
-                max-w-[60px] max-h-[30px] sm:max-w-full sm:max-h-[100px] 
-                inline-flex 
-                items-center 
-                gap-x-2 
-                text-[13px]
-                sm:text-sm 
-                font-semibold 
-                rounded-lg 
-                border 
-                border-transparent 
-                text-blue-600 
-                hover:text-blue-800 
-                focus:outline-hidden 
-                focus:text-blue-800 
-                disabled:opacity-50 
-                cursor-pointer">
-                    Download
-                </a>
-                <span v-else class="text-gray-400"><ProgressSpinner style="width: 30px; height: 30px;"></ProgressSpinner></span>
-            </td>
-            <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm font-medium">
+              <div class="flex items-center justify-between gap-x-4 w-full">
                 <button type="button" 
                 @click="deleteObject(object.name)"
                 class="
@@ -63,14 +38,55 @@
                 text-red-600 
                 hover:text-red-800 
                 focus:outline-hidden 
-                focus:text-red-800 
+                focus:text-red-800
                 disabled:opacity-50 
                 cursor-pointer
-                ">Delete</button>
+                ">
+                  <p class="pi pi-trash"></p>
+                </button>
+
+                <a
+                  v-if="fileLinks[object.name]"
+                  :href="`${fileLinks[object.name]}`"
+                  download
+                  class="
+                    break-all whitespace-normal
+                    inline-flex 
+                    items-center 
+                    text-[13px]
+                    sm:text-sm 
+                    font-semibold 
+                    rounded-lg 
+                    border 
+                    border-transparent 
+                    text-blue-600 
+                    hover:text-blue-800 
+                    focus:outline-hidden 
+                    focus:text-blue-800 
+                    disabled:opacity-50 
+                    cursor-pointer"
+                >
+                  <p class="pi pi-download"></p>
+                </a>
+
+                <span v-else class="text-gray-400">
+                  <ProgressSpinner style="width: 30px; height: 30px;"></ProgressSpinner>
+                </span>
+
+                <button @click="openPreview(object)" class="pi pi-arrow-up-right-and-arrow-down-left-from-center text-lg text-gray-500 hover:text-gray-700 cursor-pointer" style="font-size: 10px;"></button>
+              </div>
             </td>
         </tr>
     </tbody>
+    <MoreDetailsDrawer
+      v-if="selectedObject"
+      :fileLinks="fileLinks"
+      :selectedObject="selectedObject"
+      v-model:visible="visible"
+    />
 </table>
+
+
 </template>
 
 
@@ -79,12 +95,12 @@ import { ref, watch } from 'vue';
 import { usePrivateLinkStore } from '@/stores/privatelink';
 import { useConfigStore } from '@/stores/config';
 import { storeToRefs } from 'pinia';
-import { formatSize,formatDateToShortString } from '@/utils/helpers'
+import { formatSize,formatDateToShortString } from '@/utils/helpers';
 import axios from 'axios';
 
 import ProgressSpinner from 'primevue/progressspinner';
 
-
+import MoreDetailsDrawer from '@/components/layout/MoreDetailsDrawer.vue'
 // Stores
 const configStore = useConfigStore();
 const { backendUrl } = storeToRefs(configStore);
@@ -99,12 +115,22 @@ const props = defineProps({
   },
 });
 
+// drawer
+const visible = ref(false);
+// selected object for drawer
+const selectedObject = ref(null);
+
+// Preview handling
+function openPreview(object) {
+  selectedObject.value = object;
+  visible.value = true;
+}
+
 // Reactive fileLinks
 const fileLinks = ref({});
 
 // Reactive local objects list
 const localObjectsList = ref([...props.objectsList]);
-
 
 // Fetch private links for all files
 async function fetchLinksForAllFiles() {
--- src/components/sharing/SharedForMe.vue
+++ src/components/sharing/SharedForMe.vue
@@ -7,54 +7,93 @@
         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden md:table-cell">MD5 Checksum</th>
         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 md:table-cell">Shared By</th>
         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden md:table-cell">Expires At</th>
-        <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden md:table-cell">Status</th>
+        <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 md:table-cell">Status</th>
         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Download link</th>
     </tr>
     </thead>
-    <tbody v-for="(object, i) in sharedObjectsList" :key="object.name"class="divide-y divide-neutral-700 overflow-scroll">
+    <tbody v-for="(object, i) in sharedObjectsList" :key="object.name" class="divide-y divide-neutral-700 overflow-scroll">
         <tr>
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm font-medium">{{ object.file_name }}</td>
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm hidden md:table-cell">{{ object.file_type }}</td>
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm hidden md:table-cell">{{ object.md5_checksum }}</td>
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm md:table-cell">{{ object.shared_by }}</td>
             <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm hidden md:table-cell">{{ formatDateToShortString(object.expires_at) }}</td>
-            <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm hidden md:table-cell">{{ isExpired(object.expires_at) ? "Expired" : "Active" }}</td>
-            <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm">
-                <a
-                    :href="`${backendUrl}/d/${object.sharing_token}`"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    class="
-                    break-all whitespace-normal
-                    max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] 
-                    inline-flex 
-                    items-center 
-                    gap-x-2 
-                    text-sm 
-                    font-semibold 
-                    rounded-lg 
-                    border 
-                    border-transparent 
-                    text-blue-600 
-                    hover:text-blue-800 
-                    focus:outline-hidden 
-                    focus:text-blue-800 
-                    disabled:opacity-50 
-                    cursor-pointer
-                    "
-                >
-                    Download
-                </a>
+            <td class="text-center px-2 py-2 sm:px-6 sm:py-4 break-all whitespace-normal max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] text-sm md:table-cell"
+                :class="[isExpired(object.expires_at) ? 'text-red-500' : 'text-green-500']"
+            >
+                
+                <p class="pi pi-circle-fill" style="font-size: 10px;"></p>
+                <p class="hidden sm:flex">{{ isExpired(object.expires_at) ? "Expired" : "Active" }}</p>
+            </td>
+
+            <td class="px-2 py-2 sm:px-6 sm:py-4">
+                <div class="flex justify-around items-center">
+                    <a
+                        :href="`${backendUrl}/d/${object.sharing_token}`"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        class="
+                        break-all whitespace-normal
+                        max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] 
+                        items-center 
+                        gap-x-2 
+                        text-sm 
+                        font-semibold 
+                        rounded-lg 
+                        border 
+                        border-transparent 
+                        text-blue-600 
+                        hover:text-blue-800 
+                        focus:outline-hidden 
+                        focus:text-blue-800 
+                        disabled:opacity-50 
+                        cursor-pointer
+                        "
+                    >
+                        <p class="pi pi-download"></p>
+                    </a>
+
+
+                    <button class="
+                        pi pi-link
+                        break-all whitespace-normal
+                        max-w-[40px] max-h-[30px] sm:max-w-full sm:max-h-[100px] 
+                        items-center 
+                        gap-x-2 
+                        text-sm 
+                        font-semibold 
+                        rounded-lg 
+                        border 
+                        border-transparent 
+                        text-white-600
+                        hover:text-blue-400 
+                        focus:outline-hidden 
+                        focus:text-blue-800 
+                        disabled:opacity-50 
+                        cursor-pointer" 
+                        @click="copyToClipboard(`${backendUrl}/d/${object.sharing_token}`)"
+                    ></button>
+                </div>
             </td>
         </tr>
     </tbody>
 </table>
+
+<PopUp
+    :message="popupMessage"
+    :type="popupType"
+    :show="popupVisible"
+    :duration="1000"
+    @hide="popupVisible = false"
+/>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useConfigStore } from '@/stores/config';
 import { storeToRefs } from 'pinia';
 import { formatDateToShortString, isExpired } from '@/utils/helpers';
+import PopUp from '@/components/layout/PopUp.vue';
 
 const configStore = useConfigStore();
 const { backendUrl } = storeToRefs(configStore);
@@ -65,5 +104,22 @@ defineProps({
         required: true,
     }
 })
+
+const textRef = ref(null);
+const popupVisible = ref(false);
+const popupMessage = ref('');
+const popupType = ref('success');
+
+const copyToClipboard = async (text) => {
+    try {
+        await navigator.clipboard.writeText(text);
+        popupMessage.value = 'Copied to clipboard!';
+        popupType.value = 'success';
+    } catch (err) {
+        popupMessage.value = 'Failed to copy';
+        popupType.value = 'error';
+    }
+    popupVisible.value = true;
+}
 
 </script>
